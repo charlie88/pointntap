80e89545480ef14816dcceffa93f33c118efc34a
James Hornitzky <james.hornitzky@gmail.com>
2016-01-07 22:11:36 +1100

    Initial Commit

---
+++ pointntap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pointntap
+//
+//  Created by James Hornitzky on 7/01/2016.
+//  Copyright © 2016 James Hornitzky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pointntap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pointntap
+//
+//  Created by James Hornitzky on 7/01/2016.
+//  Copyright © 2016 James Hornitzky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
